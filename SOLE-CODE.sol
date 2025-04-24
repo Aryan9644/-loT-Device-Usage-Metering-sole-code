938ca5bfac4806cc99d1f659451e27565471f2e6
Aryan9644 <aryansaxena9977@gmail.com>
2025-04-24 19:46:13 +0530

    Update SOLE-CODE.sol

--- SOLE-CODE.sol
+++ SOLE-CODE.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+ // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract IoTDeviceMeter {
@@ -20,15 +20,43 @@ contract IoTDeviceMeter {
         owner = msg.sender;
     }
 
-    // Function to record usage for a device
+    // Record usage for a device
     function recordUsage(address device, uint256 usage) external onlyOwner {
         deviceUsage[device].totalUsage += usage;
         deviceUsage[device].lastUpdated = block.timestamp;
     }
 
-    // Function to retrieve the usage of a device
+    // Get the usage of a device
     function getUsage(address device) external view returns (uint256, uint256) {
         UsageRecord memory record = deviceUsage[device];
         return (record.totalUsage, record.lastUpdated);
+    }
+
+    // Reset usage for a device
+    function resetUsage(address device) external onlyOwner {
+        deviceUsage[device].totalUsage = 0;
+        deviceUsage[device].lastUpdated = block.timestamp;
+    }
+
+    // Remove a device record
+    function removeDevice(address device) external onlyOwner {
+        delete deviceUsage[device];
+    }
+
+    // Manually set usage for a device
+    function setUsage(address device, uint256 usage) external onlyOwner {
+        deviceUsage[device].totalUsage = usage;
+        deviceUsage[device].lastUpdated = block.timestamp;
+    }
+
+    // Transfer contract ownership
+    function transferOwnership(address newOwner) external onlyOwner {
+        require(newOwner != address(0), "Invalid address");
+        owner = newOwner;
+    }
+
+    // Check if a device has a usage record
+    function isDeviceRegistered(address device) external view returns (bool) {
+        return deviceUsage[device].lastUpdated != 0;
     }
 }
